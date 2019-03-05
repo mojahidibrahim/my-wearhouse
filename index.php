d59d89ffc2954643dc3e1b098cd7bd501a142183
mojahidibrahim <mojahidibrahim888@gmail.com>
2019-03-05 14:19:51 +0300

    my wearhouse

---
+++ index.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" dir="rtl">
+
+<head>
+<meta http-equiv="Content-Type" content="text/html"; charset="utf-8" />
+<title>المستودع الرقمي جامعة السودان المفتوحة</title>
+<link rel="stylesheet" href="style index.css" media="all"/>
+
+<body background="333.png"/>
+
+<div class="main_wrapper">
+<div>
+
+<div class="header_wrapper">
+<img src="ous1.png" width="1360" height="110" style="float:center;"/>
+</div>
+
+<font color="white" size="4" face="Courier New, Courier, monospace" >
+<marquee scrollamount="9" behavior="" direction="right">
+<strong >
+ مرحبا بكم في مستودع جامعة السودان المفتوحة الرقمي للبحوث والرسائل الجامعية
+</strong>
+</marquee>
+</font>
+<hr color="white" size="3">
+<img src="Open sudan1.jpg" width="1180" height="390" style="float:left;"/>
+<div class="social">
+<div> 
+
+<center>  
+<a href="login user.php">
+<img src="Picture2.jpg" width="160" height="40" style="float:right;"/> 
+</a>
+<br>
+<br>
+<br>
+<a href="register new student.php">
+<img src="123.jpg" width="160" height="40"  style="float:right;"/>
+</a>
+<br>
+<br>
+<br>
+<a href="login student.php">
+<img src="1234.jpg" width="160" height="40"  style="float:right;"/>
+</a>
+<br>
+<br>
+<br>
+<a href="https://www.ous.edu.sd/">
+<img src="Picture5.jpg" width="160" height="40"  style="float:right;"/>
+</a>
+<br>
+<br>
+<br>
+<a href="https://www.ous.edu.sd/portal/"> 
+<img src="Picture6.jpg"  width="160" height="40" style="float:right;"/>
+</a>
+<br>
+<br>
+<br>
+<br>
+<br>
+<center>
+ <a href="admin.php"><strong>مشرف المستودع الرقمي</strong></a><br><br></center/>
+ </a>
+ <center>
+<a href="https://24timezones.com" style="float:;" class="clock24" id="tz24-1549971698-cc15455-eyJob3VydHlwZSI6IjI0Iiwic2hvd2RhdGUiOiIxIiwic2hvd3NlY29uZHMiOiIxIiwic2hvd3RpbWV6b25lIjoiMCIsInR5cGUiOiJkIiwibGFuZyI6ImVuIn0=" title="time now" target="_blank" rel="nofollow"></a>
+<script type="text/javascript" src="//w.24timezones.com/l.js" async></script>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<footer class="footer">
+        <nav class="navbar navbar-expand-sm navbar-dark fixed-bottom bg-dark p-0">
+            <div class="container-fluid">
+                <span class="navbar-text text-muted">2019 كل الحقوق محفوظة@</span>                
+            </div>
+        </nav>
+    </footer>
+</center>
+</body>
+</html>
